11c69acdbc685fc85ef7caa6c1a39530314a039c
Matthieu Parizeau <mattparizeau@hotmail.com>
2018-01-03 14:15:10 -0500

    More on PVSData

--- PVSFormat/PVS.cs
+++ PVSFormat/PVS.cs
@@ -8,20 +8,53 @@ using BundleFormat;
 
 namespace PVSFormat
 {
+    public struct LocationData
+    {
+        public float X;
+        public float Y;
+        public float Unknown1;
+        public int Unknown2;
+
+        public override string ToString()
+        {
+            return "X: " + X + ", Y: " + Y + ", Unk1: " + Unknown1 + ", Unk2: " + Unknown2;
+        }
+    }
+
+    public struct NeighborData
+    {
+        public int NeighborIndex;
+        public uint NeighborPtr;
+        public int Type;
+        public int Unknown1;
+        public int Unknown2;
+
+        public override string ToString()
+        {
+            return "NeighborIndex: " + NeighborIndex + ", Type: " + Type + ", Unk1: " + Unknown1 + ", Unk2: " + Unknown2;
+        }
+    }
+
     public struct PVSEntry
     {
-        public ulong Ptr1;
-        public ulong Ptr2;
-        public int TrackID;
+        public uint Address;
+        public uint Ptr1;
         public int Unknown1;
-        public short Unknown2;
+        public uint Ptr2;
+        public int Unknown2;
+        public int TrackID;
+        public int Unknown3;
+        public short Unknown4;
         public short Count1;
-        public short Unknown3;
+        public short Unknown5;
         public short Count2;
-        public int Unknown4;
-        public int Unknown5;
         public int Unknown6;
         public int Unknown7;
+        public int Unknown8;
+        public int Unknown9;
+
+        public List<LocationData> LocationData;
+        public List<NeighborData> NeighborData;
 
         public override string ToString()
         {
@@ -65,20 +98,77 @@ namespace PVSFormat
             {
                 PVSEntry pvsEntry = new PVSEntry();
 
-                pvsEntry.Ptr1 = br.ReadUInt64();
-                pvsEntry.Ptr2 = br.ReadUInt64();
-                pvsEntry.TrackID = br.ReadInt32();
+                pvsEntry.Address = (uint)br.BaseStream.Position;
+                pvsEntry.Ptr1 = br.ReadUInt32();
                 pvsEntry.Unknown1 = br.ReadInt32();
-                pvsEntry.Unknown2 = br.ReadInt16();
+                pvsEntry.Ptr2 = br.ReadUInt32();
+                pvsEntry.Unknown2 = br.ReadInt32();
+                pvsEntry.TrackID = br.ReadInt32();
+                pvsEntry.Unknown3 = br.ReadInt32();
+                pvsEntry.Unknown4 = br.ReadInt16();
                 pvsEntry.Count1 = br.ReadInt16();
-                pvsEntry.Unknown3 = br.ReadInt16();
+                pvsEntry.Unknown5 = br.ReadInt16();
                 pvsEntry.Count2 = br.ReadInt16();
-                pvsEntry.Unknown4 = br.ReadInt32();
-                pvsEntry.Unknown5 = br.ReadInt32();
                 pvsEntry.Unknown6 = br.ReadInt32();
                 pvsEntry.Unknown7 = br.ReadInt32();
+                pvsEntry.Unknown8 = br.ReadInt32();
+                pvsEntry.Unknown9 = br.ReadInt32();
+
+                long pos = br.BaseStream.Position;
+
+                br.BaseStream.Position = (long)pvsEntry.Ptr1;
+
+                pvsEntry.LocationData = new List<LocationData>();
+                for (int j = 0; j < pvsEntry.Count1; j++)
+                {
+                    LocationData data = new LocationData();
+
+                    data.X = br.ReadSingle();
+                    data.Y = br.ReadSingle();
+                    data.Unknown1 = br.ReadSingle();
+                    data.Unknown2 = br.ReadInt32();
+
+                    pvsEntry.LocationData.Add(data);
+                }
+
+                br.BaseStream.Position = (long) pvsEntry.Ptr2;
+
+                pvsEntry.NeighborData = new List<NeighborData>();
+                for (int j = 0; j < pvsEntry.Count2; j++)
+                {
+                    NeighborData data = new NeighborData();
+
+                    data.NeighborIndex = -1;
+
+                    data.NeighborPtr = br.ReadUInt32();
+                    data.Type = br.ReadInt32();
+                    data.Unknown1 = br.ReadInt32();
+                    data.Unknown2 = br.ReadInt32();
+
+                    pvsEntry.NeighborData.Add(data);
+                }
+
+                br.BaseStream.Position = pos;
 
                 result.Entries.Add(pvsEntry);
+            }
+
+            for (int i = 0; i < result.Entries.Count; i++)
+            {
+                for (int k = 0; k < result.Entries[i].NeighborData.Count; k++)
+                {
+                    NeighborData data = result.Entries[i].NeighborData[k];
+                    uint ptr = data.NeighborPtr;
+                    for (int j = 0; j < result.Entries.Count; j++)
+                    {
+                        if (result.Entries[j].Address == ptr)
+                        {
+                            data.NeighborIndex = j;
+                            result.Entries[i].NeighborData[k] = data;
+                            break;
+                        }
+                    }
+                }
             }
 
             br.Close();

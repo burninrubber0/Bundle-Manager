f2a0a63ae3bd00805a79f6aa48dea8f3267aade0
Niaz <tigrexspalterlp@gmail.com>
2022-05-22 12:29:14 +0200

    Add ToBytes function to classes

--- BaseHandlers/StreetData.cs
+++ BaseHandlers/StreetData.cs
@@ -12,6 +12,13 @@ using PluginAPI;
 
 namespace BaseHandlers
 {
+
+    //48-4992 Street
+    //5020-12796 Junction
+    //12832-18232 Road
+    //18304-21304 ChallengePar
+    //21344-23840 Spans
+    //23856-29552 Exits(8) - Array Padding (Must be dividable by 16)
     public class ScoreList
     {
         public int[] maScores; //0x0	0x8	int32_t[2] maScores
@@ -22,34 +29,95 @@ namespace BaseHandlers
             maScores[0] = br.ReadInt32();
             maScores[1] = br.ReadInt32();
         }
+
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(BitConverter.GetBytes(maScores[0]));
+            bytes.Add(BitConverter.GetBytes(maScores[1]));
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public int getDataSize()
+        {
+            return ToBytes().Count();
+        }
+
+        public void Write(BinaryWriter br)
+        {
+            br.Write(ToBytes());
+        }
     }
 
     public class ChallengeData
     {
+        public long position;
         public byte[] mDirty;//0x0	0x8	BitArray<2u> mDirty		
         public byte[] mValidScore;// 0x8	0x8	BitArray<2u> mValidScores		
         public ScoreList mScoreList; //0x10	0x8	ScoreList mScoreList      ScoreList format
 
         public void Read(BinaryReader2 br)
         {
+            position = br.BaseStream.Position;
             mDirty = br.ReadBytes(8);
             mValidScore = br.ReadBytes(8);
             mScoreList = new ScoreList();
             mScoreList.Read(br);
         }
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(mDirty);
+            bytes.Add(mValidScore);
+            bytes.Add(mScoreList.ToBytes());
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public int getDataSize()
+        {
+            return ToBytes().Count();
+        }
+
+        public void Write(BinaryWriter br)
+        {
+            br.Write(ToBytes());
+        }
     }
 
     public class Exit
     {
+        public long position;
+        public int byteLength;
         public short mSpan; //0x0	0x2	SpanIndex	mSpan
         public byte[] padding; //0x2	0x2			padding
         public float mrAngle; //0x4	0x4	float_t	mrAngle	
 
         public void Read(BinaryReader2 br)
         {
+            position = br.BaseStream.Position;
             mSpan = br.ReadInt16();
             padding = br.ReadBytes(2);
             mrAngle = br.ReadSingle();
+            byteLength = getDataSize();
+        }
+
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(BitConverter.GetBytes(mSpan));
+            bytes.Add(padding);
+            bytes.Add(BitConverter.GetBytes(mrAngle));
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public int getDataSize()
+        {
+            return ToBytes().Count();
+        }
+
+        public void Write(BinaryWriter br)
+        {
+            br.Write(ToBytes());
         }
 
     }
@@ -63,6 +131,24 @@ namespace BaseHandlers
         {
             muMaxSpeedMPS = br.ReadByte();
             muMinSpeedMPS = br.ReadByte();
+        }
+
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(BitConverter.GetBytes(muMaxSpeedMPS));
+            bytes.Add(BitConverter.GetBytes(muMinSpeedMPS));
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public int getDataSize()
+        {
+            return ToBytes().Count();
+        }
+
+        public void Write(BinaryWriter br)
+        {
+            br.Write(ToBytes());
         }
     }
 
@@ -86,19 +172,69 @@ namespace BaseHandlers
             padding = br.ReadBytes(2);
             meSpanType = (ESpanType)br.ReadInt32();
         }
+
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(BitConverter.GetBytes(miRoadIndex));
+            bytes.Add(BitConverter.GetBytes(miSpanIndex));
+            bytes.Add(padding);
+            bytes.Add(BitConverter.GetBytes((int)meSpanType));
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public int getDataSize()
+        {
+            return ToBytes().Count();
+        }
+
+        public void Write(BinaryWriter br)
+        {
+            br.Write(ToBytes());
+        }
     }
 
     public class Street
     {
-
+        public long position;
         public SpanBase super_SpanBase; //0x0	0xC	SpanBase super_SpanBase      SpanBase format
         public AIInfo mAiInfo; //0xC	0x2	AIInfo mAIInfo     AIInfo format
         public byte[] padding; //0xE	0x2			padding
+
+        public void Read(BinaryReader2 br) {
+            position = br.BaseStream.Position;
+            SpanBase spanBase = new SpanBase();
+            spanBase.Read(br);
+            AIInfo info = new AIInfo();
+            info.Read(br);
+            super_SpanBase = spanBase;
+            mAiInfo = info;
+            padding = br.ReadBytes(2);
+        }
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(super_SpanBase.ToBytes());
+            bytes.Add(mAiInfo.ToBytes());
+            bytes.Add(padding);
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public int getDataSize()
+        {
+            return ToBytes().Count();
+        }
+
+        public void Write(BinaryWriter br)
+        {
+            br.Write(ToBytes());
+        }
 
     }
 
     public class Junction
     {
+        public long position;
         public SpanBase super_SpanBase;  //0x0	0xC	SpanBase super_SpanBase      SpanBase format
         public int mpaExits; //0xC	0x4	Exit* mpaExits        Exit format
         public int miExitCount; //0x10	0x4	int32_t miExitCount		
@@ -109,10 +245,65 @@ namespace BaseHandlers
         {
             exits = new List<Exit>();
         }
+
+        public void Read(BinaryReader2 br)
+        {
+            SpanBase spanBase = new SpanBase();
+            spanBase.Read(br);
+            position = br.BaseStream.Position;
+            mpaExits = br.ReadInt32();
+            miExitCount = br.ReadInt32();
+            macName = new string(br.ReadChars(16));
+            long oldPosition = br.BaseStream.Position;
+            br.BaseStream.Position = mpaExits;
+            for (int j = 0; j < miExitCount; j++)
+            {
+                Exit exit = new Exit();
+                exit.Read(br);
+                exits.Add(exit);
+            }
+            br.BaseStream.Position = oldPosition;
+            super_SpanBase = spanBase;
+        }
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(super_SpanBase.ToBytes());
+            bytes.Add(BitConverter.GetBytes(mpaExits));
+            bytes.Add(BitConverter.GetBytes(miExitCount));
+            bytes.Add(Encoding.ASCII.GetBytes(macName.PadRight(16).Substring(0, 16).ToCharArray()));
+            bytes.Add(BitConverter.GetBytes(miExitCount));
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public byte[] ExitsToBytes() {
+            List<byte[]> bytes = new List<byte[]>();
+            foreach (Exit exit in exits)
+            {
+                bytes.Add(exit.ToBytes());
+            }
+            //Add padding
+            if (bytes.SelectMany(i => i).Count() % 16 != 0)
+            {
+                bytes.Add(new byte[bytes.SelectMany(i => i).Count() + (16 - (bytes.SelectMany(i => i).Count() % 16))]);
+            }
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public int getDataSize()
+        {
+            return ToBytes().Count();
+        }
+
+        public void Write(BinaryWriter br)
+        {
+            br.Write(ToBytes());
+        }
     }
 
     public class Road
     {
+        public long position;
         public Vector3 mReferencePosition; //0x0	0xC	Vector3	mReferencePosition
         public int mpaSpans; //0xC	0x4	SpanIndex* mpaSpans	
         public long mId; //0x10	0x8	GameDB ID   mId	
@@ -131,12 +322,81 @@ namespace BaseHandlers
         {
             spans = new List<int>();
         }
+
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(BitConverter.GetBytes(mReferencePosition.X));
+            bytes.Add(BitConverter.GetBytes(mReferencePosition.Y));
+            bytes.Add(BitConverter.GetBytes(mReferencePosition.Z));
+            bytes.Add(BitConverter.GetBytes(mpaSpans));
+            bytes.Add(BitConverter.GetBytes(mId));
+            bytes.Add(BitConverter.GetBytes(miRoadLimitId0));
+            bytes.Add(BitConverter.GetBytes(miRoadLimitId1));
+            bytes.Add(Encoding.ASCII.GetBytes(macDebugName.PadRight(16).Substring(0, 16).ToCharArray()));
+            bytes.Add(BitConverter.GetBytes(mChallenge));
+            bytes.Add(BitConverter.GetBytes(miSpanCount));
+            bytes.Add(BitConverter.GetBytes(unknown));
+            bytes.Add(padding);
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public byte[] SpansToBytes() {
+            List<byte[]> bytes = new List<byte[]>();
+            foreach(int span in spans)
+            {
+                bytes.Add(BitConverter.GetBytes(span));
+            }
+            return bytes.SelectMany(i => i).ToArray();
+        }
+
+        public void Read(BinaryReader2 br) {
+            position = br.BaseStream.Position;
+            mReferencePosition = br.ReadVector3F();
+            mpaSpans = br.ReadInt32();
+            mId = br.ReadInt64();
+            miRoadLimitId0 = br.ReadInt64();
+            miRoadLimitId1 = br.ReadInt64();
+            macDebugName = Encoding.ASCII.GetString(br.ReadBytes(16));
+            mChallenge = br.ReadInt32();
+            miSpanCount = br.ReadInt32();
+            unknown = br.ReadInt32();
+            padding = br.ReadBytes(4);
+            long oldPosition = br.BaseStream.Position;
+            br.BaseStream.Position = mpaSpans;
+            for (int j = 0; j < miSpanCount; j++)
+            {
+                spans.Add(br.ReadInt32());
+            }
+            br.BaseStream.Position = oldPosition;
+        }
     }
 
     public class ChallengeParScores
     {
+        public long position;
         public ChallengeData challengeData; //0x0	0x18	ChallengeData	super_ChallengeData		ChallengeData format
         public long[] mRivals; //0x18	0x10	CgsID[2]	mRivals
+
+        public void Read(BinaryReader2 br)
+        {
+            position = br.BaseStream.Position;
+            ChallengeData data = new ChallengeData();
+            challengeData = data;
+            data.Read(br);
+            mRivals = new long[2];
+            mRivals[0] = br.ReadInt64();
+            mRivals[1] = br.ReadInt64();
+        }
+
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(challengeData.ToBytes());
+            bytes.Add(BitConverter.GetBytes(mRivals[0]));
+            bytes.Add(BitConverter.GetBytes(mRivals[1]));
+            return bytes.SelectMany(i => i).ToArray();
+        }
     }
 
     public class StreetData : IEntryData
@@ -144,9 +404,9 @@ namespace BaseHandlers
         public int miVersion; // 0x0	0x4	int32_t miVersion		6 in 1.4+
         private int miSize; //0x4	0x4	int32_t miSize	
         public int mpaStreets; //0x8	0x4	Street* mpaStreets      Street format
-        private int mpaJunctions; // 0xC	0x4	Junction* mpaJunctions        Junction format
-        private int mpaRoads; //0x10	0x4	Road* mpaRoads        Road format
-        private int mpaChallengeParScores; //0x14	0x4	ChallengeParScoresEntry* mpaChallengeParScores       ChallengeParScoresEntry format
+        public int mpaJunctions; // 0xC	0x4	Junction* mpaJunctions        Junction format
+        public int mpaRoads; //0x10	0x4	Road* mpaRoads        Road format
+        public int mpaChallengeParScores; //0x14	0x4	ChallengeParScoresEntry* mpaChallengeParScores       ChallengeParScoresEntry format
         private int miStreetCount; //0x18	0x4	int32_t miStreetCount	
         private int miJunctionCount; //0x1C	0x4	int32_t miJunctionCount		
         private int miRoadCount; //0x20	0x4	int32_t miRoadCount
@@ -161,6 +421,27 @@ namespace BaseHandlers
             junctions = new List<Junction>();
             roads = new List<Road>();
             challenges = new List<ChallengeParScores>();
+        }
+
+        public byte[] ToBytes()
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            bytes.Add(BitConverter.GetBytes(miVersion));
+            bytes.Add(BitConverter.GetBytes(miSize)); //Calculate this
+            bytes.Add(BitConverter.GetBytes(mpaStreets)); //Calculate this
+            bytes.Add(BitConverter.GetBytes(mpaJunctions)); //Calculate this
+            bytes.Add(BitConverter.GetBytes(mpaRoads)); //Calculate this
+            bytes.Add(BitConverter.GetBytes(mpaChallengeParScores)); //Calculate this
+            bytes.Add(BitConverter.GetBytes(streets.Count()));
+            bytes.Add(BitConverter.GetBytes(junctions.Count())); 
+            bytes.Add(BitConverter.GetBytes(roads.Count()));
+            bytes.Add(streets.SelectMany(i => i.ToBytes()).ToArray());
+            bytes.Add(junctions.SelectMany(i => i.ToBytes()).ToArray());
+            bytes.Add(roads.SelectMany(i => i.ToBytes()).ToArray());
+            bytes.Add(challenges.SelectMany(i => i.ToBytes()).ToArray());
+            bytes.Add(roads.SelectMany(i => i.SpansToBytes()).ToArray());
+            bytes.Add(junctions.SelectMany(i => i.ExitsToBytes()).ToArray());
+            return bytes.SelectMany(i => i).ToArray();
         }
 
         private void Clear()
@@ -199,18 +480,13 @@ namespace BaseHandlers
             miJunctionCount = br.ReadInt32();
             miRoadCount = br.ReadInt32();
 
+
             br.BaseStream.Position = mpaStreets;
 
             for (int i = 0; i < miStreetCount; i++)
             {
                 Street street = new Street();
-                SpanBase spanBase = new SpanBase();
-                spanBase.Read(br);
-                AIInfo info = new AIInfo();
-                info.Read(br);
-                street.super_SpanBase = spanBase;
-                street.mAiInfo = info;
-                street.padding = br.ReadBytes(2);
+                street.Read(br);
                 streets.Add(street);
             }
 
@@ -219,23 +495,7 @@ namespace BaseHandlers
             for (int i = 0; i < miJunctionCount; i++)
             {
                 Junction junction = new Junction();
-                SpanBase spanBase = new SpanBase();
-                spanBase.Read(br);
-                junction.mpaExits = br.ReadInt32();
-                junction.miExitCount = br.ReadInt32();
-                junction.macName = new string(br.ReadChars(16));
-                long oldPosition = br.BaseStream.Position;
-                br.BaseStream.Position = junction.mpaExits;
-                Console.Out.WriteLine(junction.miExitCount);
-
-                for (int j = 0; j < junction.miExitCount; j++)
-                {
-                    Exit exit = new Exit();
-                    exit.Read(br);
-                    junction.exits.Add(exit);
-                }
-                br.BaseStream.Position = oldPosition;
-                junction.super_SpanBase = spanBase;
+                junction.Read(br);
                 junctions.Add(junction);
             }
 
@@ -244,36 +504,16 @@ namespace BaseHandlers
             for (int i = 0; i < miRoadCount; i++)
             {
                 Road road = new Road();
-                road.mReferencePosition = br.ReadVector3F();
-                road.mpaSpans = br.ReadInt32();
-                road.mId = br.ReadInt64();
-                road.miRoadLimitId0 = br.ReadInt64();
-                road.miRoadLimitId1 = br.ReadInt64();
-                road.macDebugName = Encoding.ASCII.GetString(br.ReadBytes(16));
-                road.mChallenge = br.ReadInt32();
-                road.miSpanCount = br.ReadInt32();
-                road.unknown = br.ReadInt32();
-                road.padding = br.ReadBytes(4);
-                long oldPosition = br.BaseStream.Position;
-                br.BaseStream.Position = road.mpaSpans;
-                for (int j = 0; j < road.miSpanCount; j++)
-                {
-                    road.spans.Add(br.ReadInt32());
-                }
-                br.BaseStream.Position = oldPosition;
+                road.Read(br);
                 roads.Add(road);
             }
 
             br.BaseStream.Position = mpaChallengeParScores;
 
-            for (int i = 0; i < miJunctionCount; i++)
+            for (int i = 0; i < miRoadCount; i++)
             {
                 ChallengeParScores score = new ChallengeParScores();
-                ChallengeData data = new ChallengeData();
-                data.Read(br);
-                score.mRivals = new long[2];
-                score.mRivals[0] = br.ReadInt64();
-                score.mRivals[1] = br.ReadInt64();
+                score.Read(br);
                 challenges.Add(score);
             }
 
@@ -286,6 +526,8 @@ namespace BaseHandlers
             BinaryWriter bw = new BinaryWriter(ms);
 
             bw.Flush();
+
+            bw.Write(ToBytes());
 
             byte[] data = ms.ToArray();
 

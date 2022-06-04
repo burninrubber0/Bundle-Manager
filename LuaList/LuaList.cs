4d4e25c44272773980845a1b0b988525ab8e50d9
Niaz <tigrexspalterlp@gmail.com>
2022-06-04 14:31:22 +0200

    Clean up LuaListDataType

--- LuaList/LuaList.cs
+++ LuaList/LuaList.cs
@@ -10,34 +10,22 @@ using System;
 
 namespace LuaList
 {
-
     public class LuaList : IEntryData
     {
-        public int version { get; set; }    //0x0	0x4	int32_t Version number	1
-        [Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]
-        public byte[] padding { get; set; } //0x4	0x4			padding		
+        public int version { get; set; }    //0x0	0x4	int32_t Version number	1  0x4	0x4			padding	
         [TypeConverter(typeof(ULongHexTypeConverter))]
         public ulong CgsId { get; set; }     //0x8	0x8	CgsID List ID Encoded
-        [Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]
-        public byte[] Unk0 { get; set; }    //0x10	0x4	Unk0* Script list Unk0 format	
-        [Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]
-        public byte[] typePointer { get; set; }//0x14	0x4	char[32] * *		Types	
-        [Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]	
-        public byte[] variablePointer { get; set; } //0x18	0x4	char[32] * *		Variables
-        [ReadOnly(true), Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]
-        public int numScripts { get; set; }      //0x1C	0x4	uint32_t Num scripts		
-        [Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]
-        public int dataLength { get; set; }      //0x20	0x4	uint32_t Data length Not including padding to end
+        public byte[] Unk0;    //0x10	0x4	Unk0* Script list Unk0 format	
+        public byte[] typePointer;//0x14	0x4	char[32] * *		Types	
+        public byte[] variablePointer; //0x18	0x4	char[32] * *		Variables
+        public int dataLength;      //0x20	0x4	uint32_t Data length Not including padding to end
         public string listTitle { get; set; }   //0x24	0x80	char[128] List title		
         [Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]
         public byte numTypes { get; set; }     //0xA4	0x1	uint8_t Num types		
         [Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]
         public byte numVariables { get; set; } //0xA5	0x1	uint8_t Num variables
-        [Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]
-        public byte[] padding2 { get; set; } // 26 bytes long
         public List<LuaListEntry> entries { get; set; }
-        [Category("Undefined Datastructure"), Description("This is currently not implemented and can be ignored")]
-        public byte[] unknownData { get; set; }
+        public byte[] unknownData;
 
         public IEntryEditor GetEditor(BundleEntry entry)
         {
@@ -58,12 +46,12 @@ namespace LuaList
         public int getLengthOfHeader() { 
             List<byte[]> bytes = new List<byte[]>();
             bytes.Add(BitConverter.GetBytes(version));
-            bytes.Add(padding);
+            bytes.Add(new byte[4]);
             bytes.Add(BitConverter.GetBytes(CgsId));
             bytes.Add(Unk0);
             bytes.Add(typePointer);
             bytes.Add(variablePointer);
-            bytes.Add(BitConverter.GetBytes(numScripts));
+            bytes.Add(BitConverter.GetBytes(entries.Count));
             bytes.Add(BitConverter.GetBytes(dataLength));
             bytes.Add(Encoding.ASCII.GetBytes((listTitle.PadRight(128).Substring(0, 128).ToCharArray())));
             bytes.Add(BitConverter.GetBytes(numTypes));
@@ -77,17 +65,17 @@ namespace LuaList
             BinaryReader2 br = new BinaryReader2(ms);
 
             version = br.ReadInt32();
-            padding = br.ReadBytes(4);
+            br.ReadBytes(4);
             CgsId = br.ReadUInt64();
             Unk0 = br.ReadBytes(4);
             typePointer = br.ReadBytes(4);
             variablePointer = br.ReadBytes(4);
-            numScripts = br.ReadInt32();
+            var numScripts = br.ReadInt32();
             dataLength = br.ReadInt32();
             listTitle = br.ReadLenString(128);
             numTypes = br.ReadByte();
             numVariables = br.ReadByte();
-            padding2 = br.ReadBytes(26);
+            br.ReadBytes(26);
 
             entries = new List<LuaListEntry>();
             for (int i = 0; i < numScripts; i++) {
@@ -107,17 +95,17 @@ namespace LuaList
             MemoryStream ms = new MemoryStream();
             BinaryWriter bw = new BinaryWriter(ms);
             bw.Write(version);
-            bw.Write(padding);
+            bw.WriteUniquePadding(4);
             bw.Write(CgsId);
             bw.Write(Unk0);
             bw.Write(typePointer);
             bw.Write(variablePointer);
             bw.Write(entries.Count);
-            bw.Write(dataLength);
+            bw.Write(dataLength); //Calculate this
             bw.Write(listTitle.PadRight(128).Substring(0, 128).ToCharArray());
             bw.Write(numTypes);
             bw.Write(numVariables);
-            bw.Write(padding2);
+            bw.WriteUniquePadding(26); 
             foreach (LuaListEntry luaentry in entries)
             {
                 luaentry.Write(bw);

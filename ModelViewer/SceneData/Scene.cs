fd81894e80774893cfbee533a6a3aacaafde323c
burninrubber0 <burninrubber0@gmail.com>
2023-05-03 18:21:09 -0400

    Remove more unused using declarations

--- ModelViewer/SceneData/Scene.cs
+++ ModelViewer/SceneData/Scene.cs
@@ -4,11 +4,8 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 using OpenTK.Mathematics;
 
 namespace ModelViewer.SceneData

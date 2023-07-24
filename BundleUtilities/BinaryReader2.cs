df80edd3e7fcb4d8c1ff9d38c9c8333494b2f325
burninrubber0 <burninrubber0@gmail.com>
2023-07-23 23:17:11 -0400

    Use BinaryPrimitives in BinaryReader2

--- BundleUtilities/BinaryReader2.cs
+++ BundleUtilities/BinaryReader2.cs
@@ -1,112 +1,88 @@
-using System;
+using System.Buffers.Binary;
 using System.IO;
 using System.Numerics;
-using System.Text;
 
 namespace BundleUtilities
 {
     public class BinaryReader2 : BinaryReader
     {
-        public bool BigEndian { get; set; }
+        public bool BigEndian { get; set; } = false;
 
         public BinaryReader2(Stream input) : base(input)
         {
-            BigEndian = false;
-        }
-
-        public BinaryReader2(Stream input, Encoding encoding) : base(input, encoding)
-        {
-            BigEndian = false;
-        }
-
-        public BinaryReader2(Stream input, Encoding encoding, bool leaveOpen) : base(input, encoding, leaveOpen)
-        {
-            BigEndian = false;
-        }
-
-        private bool ShouldFlip()
-        {
-            return (BigEndian && BitConverter.IsLittleEndian) || (!BigEndian && !BitConverter.IsLittleEndian);
+            
         }
 
         public override short ReadInt16()
         {
-            var data = base.ReadBytes(2);
-            if (data.Length < 2)
+            if (BaseStream.Length - BaseStream.Position < 2)
                 throw new EndOfStreamException();
-            if (ShouldFlip())
-                Array.Reverse(data);
-            return BitConverter.ToInt16(data, 0);
+            if (BigEndian)
+                return BinaryPrimitives.ReadInt16BigEndian(ReadBytes(2));
+            return BinaryPrimitives.ReadInt16LittleEndian(ReadBytes(2));
         }
 
         public override int ReadInt32()
         {
-            var data = base.ReadBytes(4);
-            if (data.Length < 4)
+            if (BaseStream.Length - BaseStream.Position < 4)
                 throw new EndOfStreamException();
-            if (ShouldFlip())
-                Array.Reverse(data);
-            return BitConverter.ToInt32(data, 0);
+            if (BigEndian)
+                return BinaryPrimitives.ReadInt32BigEndian(ReadBytes(4));
+            return BinaryPrimitives.ReadInt32LittleEndian(ReadBytes(4));
         }
 
         public override long ReadInt64()
         {
-            var data = base.ReadBytes(8);
-            if (data.Length < 8)
+            if (BaseStream.Length - BaseStream.Position < 8)
                 throw new EndOfStreamException();
-            if (ShouldFlip())
-                Array.Reverse(data);
-            return BitConverter.ToInt64(data, 0);
+            if (BigEndian)
+                return BinaryPrimitives.ReadInt64BigEndian(ReadBytes(8));
+            return BinaryPrimitives.ReadInt64LittleEndian(ReadBytes(8));
         }
 
         public override ushort ReadUInt16()
         {
-            var data = base.ReadBytes(2);
-            if (data.Length < 2)
+            if (BaseStream.Length - BaseStream.Position < 2)
                 throw new EndOfStreamException();
-            if (ShouldFlip())
-                Array.Reverse(data);
-            return BitConverter.ToUInt16(data, 0);
+            if (BigEndian)
+                return BinaryPrimitives.ReadUInt16BigEndian(ReadBytes(2));
+            return BinaryPrimitives.ReadUInt16LittleEndian(ReadBytes(2));
         }
 
         public override uint ReadUInt32()
         {
-            var data = base.ReadBytes(4);
-            if (data.Length < 4)
+            if (BaseStream.Length - BaseStream.Position < 4)
                 throw new EndOfStreamException();
-            if (ShouldFlip())
-                Array.Reverse(data);
-            return BitConverter.ToUInt32(data, 0);
+            if (BigEndian)
+                return BinaryPrimitives.ReadUInt32BigEndian(ReadBytes(4));
+            return BinaryPrimitives.ReadUInt32LittleEndian(ReadBytes(4));
         }
 
         public override ulong ReadUInt64()
         {
-            var data = base.ReadBytes(8);
-            if (data.Length < 8)
+            if (BaseStream.Length - BaseStream.Position < 8)
                 throw new EndOfStreamException();
-            if (ShouldFlip())
-                Array.Reverse(data);
-            return BitConverter.ToUInt64(data, 0);
+            if (BigEndian)
+                return BinaryPrimitives.ReadUInt64BigEndian(ReadBytes(8));
+            return BinaryPrimitives.ReadUInt64LittleEndian(ReadBytes(8));
         }
 
         public override float ReadSingle()
         {
-            var data = base.ReadBytes(4);
-            if (data.Length < 4)
+            if (BaseStream.Length - BaseStream.Position < 4)
                 throw new EndOfStreamException();
-            if (ShouldFlip())
-                Array.Reverse(data);
-            return BitConverter.ToSingle(data, 0);
+            if (BigEndian)
+                return BinaryPrimitives.ReadSingleBigEndian(ReadBytes(8));
+            return BinaryPrimitives.ReadSingleLittleEndian(ReadBytes(4));
         }
 
         public override double ReadDouble()
         {
-            var data = base.ReadBytes(8);
-            if (data.Length < 8)
+            if (BaseStream.Length - BaseStream.Position < 8)
                 throw new EndOfStreamException();
-            if (ShouldFlip())
-                Array.Reverse(data);
-            return BitConverter.ToDouble(data, 0);
+            if (BigEndian)
+                return BinaryPrimitives.ReadDoubleBigEndian(ReadBytes(8));
+            return BinaryPrimitives.ReadDoubleLittleEndian(ReadBytes(8));
         }
 
         public Vector4 ReadVector4()
